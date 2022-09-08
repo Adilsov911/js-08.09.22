b660bcff98e73d4ff12f6950d7ab0b55a3d0b84c
Adilsov911 <adiles@code.edu.az>
2022-09-08 23:45:53 +0400

    homework

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+function Input(){
+    while(true){
+        var x = prompt('yasini qeyd et')
+    if(x.length <= 0 || x.indexOf(' ') >= 0){
+        alert('Can not be empty')
+    }
+    else if(isNaN(x)){
+        alert('Eded daxil et')
+    }
+    else if(x < 18){
+        alert('18 yasdan kicikdir')
+    }
+    else{
+        alert('Qeydiyyat Tamamlandi')
+        break;
+    }
+    }
+    function Show() {
+        var value = document.getElementById("inp").value;
+        console.log(value);
+    }
+}
+function Show() {
+    var value = document.getElementById("inp").value;
+    console.log(value);
+    var x = value.slice(1,2)
+    if (x<4) {
+        if (x==1) {
+            document.body.style.backgroundColor = "yellow"
+        }
+        if (x==2) {
+            document.body.style.backgroundColor = "red"
+        }
+        if (x==3) {
+            document.body.style.backgroundColor = "Black"
+        }
+        if (x==0) {
+            alert("bele qrup yoxdur")
+        }
+    }
+}
